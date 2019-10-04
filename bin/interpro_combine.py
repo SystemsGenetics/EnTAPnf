e791cfe078b5f261292e30d965c99c0e2bb451a1
Stephen Ficklin <spficklin@gmail.com>
2019-10-04 12:23:41 -0700

    Refactoring of interpro_combine.py

--- bin/interpro_combine.py
+++ bin/interpro_combine.py
@@ -1,71 +1,91 @@
 #!/usr/bin/env python3
 
 
+"""
+Overall description of what this module does.
+
+.. module:: Annotater
+    :platform: UNIX, Linux
+    :synopsis: add a short synoposis.
+
+"""
+
 import glob
 import pandas as pd
 import numpy as np
 import re
 
-filenames = sorted(glob.glob('*.tsv'))
+def write_IPR(ipr_file, tsv_file):
+    """"
+    Retrieves the IPR terms from a TSV file and writes them to a mapping file.
 
-################## IPR_mappings
-file = open('IPR_mappings.txt','w')
-file.close()
+    :param ipr_file: the file handle to the IPR_Mapping.txt file.
+    :param tsv_file: the TSV file from InterProScan.
 
-cols = np.arange(15)
-for f in filenames:
-    table = pd.read_table(f,
-                          sep='\t',
-                          header=None,
-                          names=cols,          # to deal with some rows missing
-                          usecols=[0,11,12])
+    """
+    # The IPR results do not have a consistent number of columns. Here we creaqte
+    # an array that gets used in the read_table function below that forces
+    # the function to include 15 columns.
+    cols = np.arange(15)
 
+    # Import partial IPR results for this file and set the column anames,
+    # drop rows that has NaN in IPR column, and remove duplicates.
+    ipr_terms = pd.read_csv(tsv_file, sep='\t', header=None, names=cols, usecols=[0,11,12])
+    ipr_terms.columns = ["Gene", "IPR", "Description"]
+    ipr_terms = ipr_terms.dropna(subset=["IPR"])
+    ipr_terms = ipr_terms.drop_duplicates(keep='first')
 
-    table.columns = ["Gene", "IP", "Description"]
-
-    # remove rows with NaN
-    table = table.dropna(subset= ["IP"])  # drop only the columns that has NaN in IPR column ???????????
-
-    # remove duplicates
-    table = table.drop_duplicates(keep='first')
-
-    # removes _[[:digit]] at the end of the gene/transcript names
-    table["Gene"] = table["Gene"].str.replace(r'^(.*?)_\d+', r'\1')
-
-#     for i in table[0].index:
-#         table[0][i] = re.sub(r'^(.*?)_\d+',r'\1',table[0][i])  ### rsplit option?? -
-
-    #break
-
-    #print(table)
-    with open('IPR_mappings.txt', 'a') as txtfile:
-        GO_terms = table.to_csv(txtfile,sep='\t',header = False,index=False)
+    # Removes the training ID number that INterProScan adds to the sequence
+    ipr_terms["Gene"] = ipr_terms["Gene"].str.replace(r'^(.*?)_\d+', r'\1')
+    ipr_terms.to_csv(ipr_file, sep="\t", mode='a', header=False, index=False)
 
 
+def write_GO(go_file, tsv_file):
+    """"
+    Retrieves the GO terms from a TSV file and writes them to a mapping file.
+
+    :param go_file: the file handle to the GO_Mapping.txt file.
+    :param tsv_file: the TSV file from InterProScan.
+    """
+
+    # The IPR results do not have a consistent number of columns. Here we creaqte
+    # an array that gets used in the read_table function below that forces
+    # the function to include 15 columns.
+    cols = np.arange(15)
+
+    # Import partial GO results for this file and set the column anames,
+    # drop rows that has NaN in IPR column, and remove duplicates.
+    go_terms = pd.read_table(tsv_file,sep='\t',header=None,names=cols,usecols=[0,13])
+    go_terms.columns = ["Gene", "GO"]
+    go_terms = go_terms.dropna(subset=["GO"])
+    go_terms = go_terms.drop_duplicates(keep='first')
+
+    # Removes the training ID number that INterProScan adds to the sequence
+    go_terms["Gene"] = go_terms["Gene"].str.replace(r'^(.*?)_\d+', r'\1')
+
+    print(go_terms)
 
 
-############### GO_mappings
-################## GO_mappings
+if __name__ == "__main__":
 
-cols = np.arange(15)
-for f in filenames:
-    table = pd.read_table(f,sep='\t',header=None,names=cols,usecols=[0,13])
-    table.columns = ["Gene","GO"]
+    # Get a list of all TSV files.
+    tsv_filenames = sorted(glob.glob('*.tsv'))
 
-    # remove rows with NaN
-    table = table.dropna()
+    # Open the IPR_mappintgs.txt file where the final IPR mappings will be stored.
+    ipr_file = open('IPR_mappings.txt','w')
+    go_file = open('GO_mappings.txt','w')
 
-    # remove duplicates
-    table = table.drop_duplicates(keep='first')
+    # Set the header names for the IPR_mappings.txt file
+    ipr_headers = ["Gene", "IPR", "Description"]
+    ipr_file.write("\t".join(ipr_headers))
+    ipr_file.write("\n")
 
-    # removes _[[:digit]] at the end of the genenames
-    for i in table[0].index:
-         = re.sub(r'^(.*?)_\d+',r'\1',table[0][i])
-        table[13][i].split('|')
-    # separating the different GO terms and assigning them to the gene/transcript
+    # Set the header names for the GO_mappings.txt file
+    go_headers = ["Gene", "GO"]
+    go_file.write("\t".join(go_headers))
+    go_file.write("\n")
 
-
-
-    #print(table)
-    #with open('IPR_mappings.txt', 'a') as txtfile:
-    #    table.to_csv(txtfile,sep='\t',header = False,index=False)
+    # Iterate through each of the TSV files and pull out the IPR mappings.
+    for tsv_file in tsv_filenames:
+       write_IPR(ipr_file, tsv_file)
+       write_GO(go_file, tsv_file)
